78927cd62f139a34e6b3924333e2a336866532a8
Cereneum <49323190+Cereneum@users.noreply.github.com>
2019-04-11 01:20:57 -0500

    Update CereneumData.sol

--- contracts/CereneumData.sol
+++ contracts/CereneumData.sol
@@ -6,16 +6,16 @@ import "./SafeMath.sol";
 
 contract CereneumData is ERC20
 {
-	using SafeMath for uint256;
+  using SafeMath for uint256;
 
   //Launch timestamp of contract used to track how long contract has been running
   uint256 internal m_tContractLaunchTime;
 
-	//Root hashes of the 5 UTXO Merkle trees. Used to verify claims.
+  //Root hashes of the 5 UTXO Merkle trees. Used to verify claims.
   //0=BTC, 1=BCH, 2=BSV, 3=ETH, 4=LTC
   bytes32[5] internal m_hMerkleTreeRootsArray;
 
-	//Total number of UTXO's at snapshot. Used for calculating bonus rewards.
+  //Total number of UTXO's at snapshot. Used for calculating bonus rewards.
   uint256 internal m_nUTXOCountAtSnapshot;
 
   //Maximum number of redeemable coins at snapshot.
@@ -25,7 +25,7 @@ contract CereneumData is ERC20
   //That has the whale penalties applied (lowering claimable supply)
   uint256 public m_nAdjustedMaxRedeemable;  //TODO ON LAUNCH DAY
 
-	//Genesis Address
+  //Genesis Address
   address internal m_genesis;
 
   //Store the BTC ratios for BCH, BSV, ETH and LTC
@@ -34,8 +34,8 @@ contract CereneumData is ERC20
   enum AddressType { LegacyUncompressed, LegacyCompressed, SegwitUncompressed, SegwitCompressed }
   enum BlockchainType { Bitcoin, BitcoinCash, BitcoinSV, Ethereum, Litecoin }
 
-	//Track how many tokens and UTXOs have been redeemed.
-	//These are used for calculating bonus rewards.
+  //Track how many tokens and UTXOs have been redeemed.
+  //These are used for calculating bonus rewards.
   uint256 public m_nTotalRedeemed = 0;
   uint256 public m_nRedeemedCount = 0;
 
@@ -48,46 +48,46 @@ contract CereneumData is ERC20
 
   //Daily data
   struct DailyDataStuct
-	{
+  {
     uint256 nPayoutAmount;
     uint256 nTotalStakeShares;
   }
 
-	//Map to store daily historical data. Could have been array instead of map
-	//but continuously adding to an array makes gas costs tricky.
+  //Map to store daily historical data. Could have been array instead of map
+  //but continuously adding to an array makes gas costs tricky.
   mapping(uint256 => DailyDataStuct) public m_dailyDataMap;
 
   //Stakes Storage
   struct StakeStruct
-	{
+  {
     uint256 nAmountStaked;
     uint256 nSharesStaked;	//Get bonus shares for longer stake times
-		uint256 nCompoundedPayoutAccumulated;
+    uint256 nCompoundedPayoutAccumulated;
     uint256 tLockTime;
     uint256 tEndStakeCommitTime;
-		uint256 tLastCompoundedUpdateTime;
+    uint256 tLastCompoundedUpdateTime;
     uint256 tTimeRemovedFromGlobalPool;
-		uint8 nVotedOnMultiplier;
-		bool bIsInGlobalPool;
+    uint8 nVotedOnMultiplier;
+    bool bIsInGlobalPool;
     bool bIsLatePenaltyAlreadyPooled;
   }
 
-	//Map of addresses to StakeStructs.
+  //Map of addresses to StakeStructs.
   mapping(address => StakeStruct[]) public m_staked;
 
-	//Accumulated early/late unstake penalties to go into next staker pool as rewards
+  //Accumulated early/late unstake penalties to go into next staker pool as rewards
   uint256 internal m_nEarlyAndLateUnstakePool;
 
-	//Track the number of staked tokens and shares
+  //Track the number of staked tokens and shares
   uint256 public m_nTotalStakedTokens;
   uint256 public m_nTotalStakeShares;	//TODO: Do we want stake shares to be known?
 
-	//The latest interest multiplier voted on by the majority of the staker pool
+  //The latest interest multiplier voted on by the majority of the staker pool
   uint8 public m_nInterestMultiplier = 1;
 
-	//The number of stake shares voting for each interest multiplier
-	//1 keeps the base 5% interest (minimum), 2 is 10%, ... 10 is 50% (maximum)
-	mapping(uint8 => uint256) public m_votingMultiplierMap;	//TODO: Do we want votes to be public knowledge?
+  //The number of stake shares voting for each interest multiplier
+  //1 keeps the base 5% interest (minimum), 2 is 10%, ... 10 is 50% (maximum)
+  mapping(uint8 => uint256) public m_votingMultiplierMap;	//TODO: Do we want votes to be public knowledge?
 
   //Maximum stake time allowed
   uint256 internal constant m_nMaxStakingTime = 365 days * 5;	//years is deprecated because of leap years
